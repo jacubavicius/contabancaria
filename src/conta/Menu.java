76fc3b05283102e1a4c11c46fe224e4309afa50c
Gabriella Jacubavicius <gabriella_jacubavicius@hotmail.com>
2024-06-14 08:48:11 -0300

    3. Classe conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,12 +1,21 @@
 package conta;
 
-import java.util.*;
-import conta.util.Cores;
+import java.util.Scanner;
 
+import conta.model.Conta;
+import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		//Teste da classe conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
@@ -54,7 +63,7 @@ public class Menu {
 				System.out.println(Cores.TEXT_WHITE_BOLD + " Consultar dados da conta - por número\n\n");
 				break;
 			case 4:
-				System.out.println(Cores.TEXT_WHITE_BOLD + " Atualizar dados da conta\nn");
+				System.out.println(Cores.TEXT_WHITE_BOLD + " Atualizar dados da conta\n\n");
 				break;
 			case 5:
 				System.out.println(Cores.TEXT_WHITE_BOLD + " Apagar a conta\n\n");
